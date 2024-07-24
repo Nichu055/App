791c254eb85a953ee74bac921222e3ce15a416f7
Nichu055 <ulasiebuka2@gmail.com>
2024-07-24 10:17:13 +0100

    update

--- splash.js
+++ splash.js
@@ -4,3 +4,6 @@ setTimeout(() => {
     document.getElementById("ven").addEventListener("click", function() {
         window.location.href = "vendor/register/reg_vendor.html";
     });
+    document.getElementById("use").addEventListener("click", function() {
+        window.location.href = "user/register/user.html";
+    });
