26e54f20c692b9a8302eff56df49986860791c8b
Nichu055 <ulasiebuka2@gmail.com>
2024-07-24 09:30:23 +0100

    update

---
+++ splash.js
@@ -0,0 +1,7 @@
+setTimeout(() => {
+    document.getElementById('splash').classList.toggle('fade'); }, 2000);
+
+    document.getElementById("ven").addEventListener("click", function() {
+        window.location.href = "vendor/register/reg_vendor.html";
+    });
+    
