38abdc6b7ad56adc967503af6f07c0a66da6491a
Nichu055 <ulasiebuka2@gmail.com>
2024-07-24 22:53:52 +0100

    form

--- vendor/register/reg_vendor.js
+++ vendor/register/reg_vendor.js
@@ -35,60 +35,69 @@ const container = document.querySelector(".container"),
 
 
 
-    regForm.addEventListener('submit', handleRegistration);
+const regForm = document.getElementById('regForm');
 
-    async function handleRegistration(e) {
-        e.preventDefault();
-        try {
-            const userDetails = {
-                name: regForm.name.value.trim(),
-                email: regForm.email.value.trim(),
-                password: regForm.password.value.trim(),
-            }
-            let confirmPassword = regForm.confirmPass.value;
-            if (userDetails.email === '' || userDetails.password === '' || userDetails.name === '') {
-                throw new Error('Ensure all fields are filled');
-            }
-            if (confirmPassword !== userDetails.password) {
-                throw new Error('Password does not match');
-            }
-            
-            // Axios request
-            const res = await axios.post('http://localhost:5158/api/Vendors', userDetails, {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
+regForm.addEventListener('submit', handleRegistration);
 
-            console.log(res);
-            window.location.href = "reg_vendor.html";
-        } catch (error) {
-            console.log(error);
+async function handleRegistration(e) {
+    e.preventDefault();
+    try {
+        const userDetails = {
+            name: regForm.name.value.trim(),
+            email: regForm.email.value.trim(),
+            password: regForm.password.value.trim(),
+        };
+        let confirmPassword = regForm.confirmPass.value;
+        
+        if (userDetails.email === '' || userDetails.password === '' || userDetails.name === '') {
+            throw new Error('Ensure all fields are filled');
         }
+        if (confirmPassword !== userDetails.password) {
+            throw new Error('Password does not match');
+        }
+        
+        // Axios request
+        const res = await axios.post('http://localhost:5158/api/Vendors', userDetails, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        console.log(res);
+        window.location.href = "reg_vendor.html";
+    } catch (error) {
+        console.log(error);
     }
+}
 
     // login
     
-    logForm.addEventListener('submit', handleLogin);
+    const logForm = document.getElementById('logForm');
 
-    async function handleLogin(e) {
-        e.preventDefault();
-        try {
-            const userDetails = {
-                email: logForm.email.value.trim(),
-                password: logForm.password.value.trim(),
-            }
-            if (userDetails.email === '' || userDetails.password === '') {
-                throw new Error('Ensure all fields are filled')
-            }
-            const res = await axios.post('http://localhost:5158/api/Vendors', userDetails, {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
-            console.log(res);
-            window.location.href = "../index.html";
-        } catch (error) {
-            console.log(error);
+logForm.addEventListener('submit', handleLogin);
+
+async function handleLogin(e) {
+    e.preventDefault();
+    try {
+        const userDetails = {
+            email: logForm.email.value.trim(),
+            password: logForm.password.value.trim(),
+        };
+
+        if (userDetails.email === '' || userDetails.password === '') {
+            throw new Error('Ensure all fields are filled');
         }
+
+        // Axios request
+        const res = await axios.post('http://localhost:5158/api/Vendors', userDetails, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        console.log(res);
+        window.location.href = "../index.html";
+    } catch (error) {
+        console.log(error);
     }
+}
